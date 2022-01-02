143e592e2fffa3e77d496716314d88ee06d6aaf7
geun9716 <crintop@gmail.com>
2022-01-02 14:34:36 +0900

    first step nestJS

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Movie } from './entities/movies.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly moviesService:MoviesService){}
+    
+    @Get()
+    getAll():Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get("search")
+    search(@Query('year') searchingYear:string){
+        return `We are searching for a movie made after: ${searchingYear}`
+    }
+    
+    @Get(":id")
+    getOne(@Param('id') mid:string):Movie {
+        return this.moviesService.getOne(mid);
+    }
+
+    @Post() 
+    create(@Body() movieData) {
+        console.log(movieData)
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete(":id")
+    remove(@Param('id') mid:string) {
+        return this.moviesService.deleteOne(mid);
+    }
+
+    @Patch(":id")
+    patch(@Param('id') mid:string, @Body() updateData) {
+        return this.moviesService.update(mid, updateData);
+    }
+}
